9ea7c01b8ac1ca31550af558dd962232a059841a
Heng <sivmiosu@gmail.com>
2026-03-15 21:20:05 +0700

    Fix a bit more

--- apps/react-client/src/pages/Dashboard.tsx
+++ apps/react-client/src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <MainNavigation activeId="home" />
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex flex-col flex-1 min-w-0">
         <DashboardClient />
       </div>
     </div>
